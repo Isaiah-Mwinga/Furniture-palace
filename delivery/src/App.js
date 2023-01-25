dd7f2e87faa6cf7b9d75f6a7048569ab65f847af
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-25 22:35:14 +0300

    food delivery

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+      <h1>Delivery</h1>
       </header>
     </div>
   );
