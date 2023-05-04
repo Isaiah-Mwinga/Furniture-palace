11cfd93924fe44a0d3df3fb5e142c5a4c40d3f5c
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-04 15:30:18 +0300

    furniture

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+//import data
+import { hero } from '../data';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const {title, subtitle, buttonText} = hero;
+  return (
+    <section className='h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:bg-center lg:mb-28'>
+      <div className=' container mx-auto text-center'>
+        {/* title*/}
+        <h1>
+          
+        </h1>
+        hero
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
