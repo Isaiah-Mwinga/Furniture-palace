24051339fc75986c489fd29211a8b1ee9c516315
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-05 10:22:57 +0300

    furniture

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,10 +9,9 @@ const Hero = () => {
     <section className='h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:bg-center lg:mb-28'>
       <div className=' container mx-auto text-center'>
         {/* title*/}
-        <h1>
-          
+        <h1 className='text-2xl mx-auto font-semibold mb-[30px] lg:text-[64px] lg:leading-tight lg:max-w-[888px]'>
+          {title}
         </h1>
-        hero
       </div>
     </section>
   );
