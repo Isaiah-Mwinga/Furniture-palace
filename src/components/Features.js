762aaa60301f62845fdee2b9fe5df338019cf024
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-05 14:14:03 +0300

    furniture

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+//import data
+import { features } from '../data';
+
 const Features = () => {
-  return <div>Features</div>;
+  //destructure features
+  const {title, subtitle, image, buttonText, items} = features;
+  return (
+    <section className='features'></section>
+  );
 };
 
 export default Features;
