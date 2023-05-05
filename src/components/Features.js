5c5b0d48d16ae8d6d5ac2e9f9551474ef65133c7
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-05 14:34:58 +0300

    furniture

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,7 +7,34 @@ const Features = () => {
   //destructure features
   const {title, subtitle, image, buttonText, items} = features;
   return (
-    <section className='features'></section>
+    <section className='section'>
+      <div>
+        {/* image */}
+        <div>
+          <img src={image.type} alt='' />
+        </div>
+        {/* text */}
+        <div>
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+          {/* items */}
+          <div>
+            {items.map((item, index) => {
+                const { title, subtitle, icon } = item;
+                return (
+                  <div>
+                    <div>{icon}</div>
+                    <div>
+                      <h3>{title}</h3>
+                      <p>{subtitle}</p>
+                    </div>  
+                  </div>
+                );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
