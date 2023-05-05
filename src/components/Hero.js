6b9cfaef51ed8634bb2d2f837c617ecefee30abb
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-05 12:34:13 +0300

    furniture

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,9 @@ import React from 'react';
 //import data
 import { hero } from '../data';
 
+//import components
+import Stats from '../components/Stats';
+
 const Hero = () => {
   const {title, subtitle, buttonText} = hero;
   return (
@@ -12,6 +15,18 @@ const Hero = () => {
         <h1 className='text-2xl mx-auto font-semibold mb-[30px] lg:text-[64px] lg:leading-tight lg:max-w-[888px]'>
           {title}
         </h1>
+        {/* subtitle*/}
+        <h2 className='mb-[30px] max-w-[627px] mx-auto lg:mb-[65px] lg:text-xl'>
+          {subtitle}
+        </h2>
+        {/* button*/}
+        <button className='bg-[rgba(255,255,255,0.4)] hover:bg-[rgba(255,255,255,0.5)] px-[35px] py-[9px] mb-[160px] text-xl rounded-md backdrop-blur-md transition lg:px-[80px] lg:py-[160px] lg:mb-[194px]'>
+          {buttonText}
+        </button>
+        {/* stats*/}
+        <div className='-top-[70px] relative'>
+          <Stats />
+        </div>
       </div>
     </section>
   );
