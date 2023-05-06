99d6fa0489fd56587d2ddbdada3d9a6dde411843
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-06 16:16:41 +0300

    furniture

--- src/components/NewItems.js
+++ src/components/NewItems.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 //import data
-import { newInStore} from '../data';
+import { newInStore } from '../data';
+//import components
+import NewItemsSlider from './NewItemsSlider';
 
 const NewItems = () => {
   //destructuring newInStore
-  const { title, subtitle, link, icon} = newInStore;
+  const { title, subtitle, link, icon } = newInStore;
   return (
     <section className='section'>
       <div className='container mx-auto'>
-        {/* text */}
+        <div>
+          {/* text */}
+          <div>
+            <h2 className='title'>{title}</h2>
+            <p className='subtitle'>{subtitle}</p>
+            <div>
+              <a href='#'>{link}</a>
+            </div>
+          </div>
+          {/* slider */}
+          <NewItemsSlider />
+        </div>
       </div>
     </section>
   );
