5ff54700a1a1d59b2487f106ee171d8916228990
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-06 11:32:30 +0300

    furniture

--- src/components/NewItems.js
+++ src/components/NewItems.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+//import data
+import { newInStore} from '../data';
+
 const NewItems = () => {
-  return <div>NewItems</div>;
+  //destructuring newInStore
+  const { title, subtitle, link, icon} = newInStore;
+  return (
+    <section className='section'>
+      <div className='container mx-auto'>
+        {/* text */}
+      </div>
+    </section>
+  );
 };
 
 export default NewItems;
