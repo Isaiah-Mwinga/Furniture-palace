15aa7760f4288101248938449ec76bdb2cc202c4
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-08 12:22:48 +0300

    furniture palace

--- src/components/FeaturesSecond.js
+++ src/components/FeaturesSecond.js
@@ -5,22 +5,21 @@ import { features } from '../data';
 
 const FeaturesSecond = () => {
   // destructure data
-  const { title, subtitle, items, image } = features.feature2;
+  const { title, subtitle,  image } = features.feature2;
   return (
     <section className='section'>
     <div className='container mx-auto'>
       <div className='flex flex-col lg:flex-row lg:gap-x-[100px]'>
-        {/* image */}
-        <div className='flex-1 order-1 lg:-order-1'>
-          <img src={image.type} alt='' />
-        </div>
         {/* text */}
         <div className='flex-1 flex flex-col justify-end'>
           <h2 className='title'>{title}</h2>
           <p className='subtitle'>{subtitle}</p>
-          {/* items */}
-          
         </div>
+        {/* image */}
+        <div className='flex-1'>
+          <img src={image.type} alt='' />
+        </div>
+        
       </div>
     </div>
   </section>
