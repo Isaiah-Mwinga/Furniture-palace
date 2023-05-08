cfd6591368ea250c9d955edc0a377b1036a9bec0
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-08 12:36:05 +0300

    furniture palace

--- src/components/ProductSlider.js
+++ src/components/ProductSlider.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+//import swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '../slider.css';
+
+// import required swiper modules
+import {Navigation, Pagination} from 'swiper';
+
+//import data
+import { products } from '../data';
+
+//import icons
+import {HiPlus} from 'react-icons/hi';
+
 const ProductSlider = () => {
   return <div>ProductSlider</div>;
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+//import data
+import { products } from '../data';
+
+//import components
+import ProductSlider from  '../components/ProductSlider';
+
 const Products = () => {
-  return <div>Products</div>;
+  // destructure products
+  const { title, subtitle} = products;
+  return (
+    <section className='section'>
+      <div className='container mx-auto'>
+        <div>
+          <h2 className='title'>{title}</h2>
+          <p className='subtitle'>{subtitle}</p>
+        </div>
+        <ProductSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Products;
