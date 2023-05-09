6eced0a7565fd257fbc28001f997213e7ed6d971
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-09 12:21:01 +0300

    furniture palace

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+//import swiper
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+//import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+//import required modules
+import { Autoplay, Navigation } from 'swiper';
+
+//import data
+import {testimonial} from '../data';
+
 const TestimonialSlider = () => {
-  return <div>TestimonialSlider</div>;
+  return (
+    <Swiper>
+      {testimonial.persons.map((person, index) => {
+        //destructure person
+        const {avatar, name, occupation, message} = person;
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              <div>
+                {/* avatar */}
+                <img src={avatar.type} alt='' />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
