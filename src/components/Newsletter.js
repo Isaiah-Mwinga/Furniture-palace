39b1fc9c6d6abec68873a7bacafab938b6525709
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-10 21:41:47 +0300

    furniture palace

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -10,10 +10,10 @@ const Newsletter = () => {
     <section className='section bg-newsletter min-h-[490px]'>
       <div className='container max-auto flex justify-center lg:justify-end'>
         <div className='w-full max-w-[558px] text-center text-white lg:text-left'>
-          <h2>{title}</h2>
-          <p>{subtitle}</p>
-          <div>
-            <input type='text' />
+          <h2 className='text-4xl font-semibold max-w-[350px] mx-auto lg:mx-0 mb-5 leading-normal'>{title}</h2>
+          <p className='text-xl font-light mb-10'>{subtitle}</p>
+          <div className='flex flex-col lg:flex-row gap-[22px]'>
+            <input className='h-[60px] px-6 outline-none placeholder:text-gray text-gray-600 rounded-lg lg:flex-1' type='text' />
             <button>{buttonText}</button>
           </div>
         </div>
