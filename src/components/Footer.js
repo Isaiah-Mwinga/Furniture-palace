d34803fdb6d8e1d27ab01d888260f73bf6a653c9
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-10 22:02:18 +0300

    furniture palace

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+//import logo
+import Logo from '../assets/img/logo.svg';
+
+//import data
+import { footer } from '../data';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  //destructure footer
+  //const { title, links } = footer;
+  return (
+    <footer className='bg-primary text-white'>
+      <div className='container max-auto'>
+        <div>
+          <a href='#'>
+            <img src={Logo} alt='' />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
