1b6dea6092311fbd39047bad6b17990c5fc1b15e
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-10 13:58:41 +0300

    furniture palace

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+//import data
+import { newsletter } from '../data';
+
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  //destructure newsletter
+  const { title, subtitle, placeholder, buttonText } = newsletter;
+  return (
+    <section className='section'>
+      <div className='container max-auto'></div>
+    </section>
+  );
 };
 
 export default Newsletter;
