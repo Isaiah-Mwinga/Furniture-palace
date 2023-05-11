c50fdf41cafa9ae9ece41e3e597ce53555a3a4f9
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-11 11:30:47 +0300

    furniture palace

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,21 +12,23 @@ const Footer = () => {
   return (
     <footer className='bg-primary text-white'>
       <div className='container max-auto'>
-        <div>
-          <a href='#'>
+        <div className='flex flex-col lg:flex-row justify-between border-b border-opacity-75 border-gray-700 pb-7 lg:pb-14 mb-1'>
+          <a className='mb-6 lg:mb-0' href='#'>
             <img src={Logo} alt='' />
           </a>
           {/*social icons*/}
-          <div>
+          <div className='flex gap-x-4'>
             {footer.social.map((item, index) => {
               return (
-                <div key={index}>
+                <div className='w-12 h-12 text-2xl bg-gray-700 hover:bg-accent rounded-full flex justify-center items-center' key={index}>
                   <a href='#'>{item.icon}</a>
                 </div>
-              )
+              );
             })}
           </div>
         </div>
+        {/*copyright*/}
+        <p className='text-center'>& FurniturePalace 2023 - All rights reserved.</p>
       </div>
     </footer>
   );
