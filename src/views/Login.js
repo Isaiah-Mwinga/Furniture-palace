4e9c7bbb25ae59f03dc8faf892e82cb9067a670d
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-21 21:24:31 +0300

    furniture palace

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,8 +1,67 @@
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      setUser(data.user);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.log('Login failed:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {user && (
+        <div>
+          <h2>Welcome, {user.name}!</h2>
+        </div>
+      )}
     </div>
   );
 }
