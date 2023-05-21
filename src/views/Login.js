3898e3e8062b5d6b83b4b63c2570d75b7f27996a
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-21 21:31:06 +0300

    furniture palace

--- src/views/Login.js
+++ src/views/Login.js
@@ -34,34 +34,43 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {user && (
-        <div>
-          <h2>Welcome, {user.name}!</h2>
-        </div>
-      )}
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full mx-auto p-6 bg-white rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-6">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block mb-2">Email:</label>
+            <input
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2">Password:</label>
+            <input
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+        {user && (
+          <div className="mt-4">
+            <h2 className="text-lg">Welcome, {user.name}!</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
