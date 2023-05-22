879407ca22f5f20561c6037ec93ac4ac0596047f
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-22 10:19:38 +0300

    furniture

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,22 @@
 import Hero from '../components/Hero';
 import Header from '../components/Header';
+import Features from '../components/Features';
+import Footer from '../components/Footer';
+import Testimonial from '../components/Testimonial';
+import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+import NewItems from '../components/NewItems';
+import FeaturesSecond from '../components/FeaturesSecond';
+import ProductSlider from '../components/ProductSlider';
+import TestimonialSlider from '../components/TestimonialSlider';
+import NewItemsSlider from '../components/NewItemsSlider';
 
 export default function Home() {
     return (
         <div>
             <Header />
             <Hero />
+
         </div>
     )
 }
