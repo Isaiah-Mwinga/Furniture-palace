b511b1524feb217fef63db2e13fe3ee22011860f
Isaiah-Mwinga <motongori@marketforce360.com>
2023-05-22 11:47:50 +0300

    furniture

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -5,8 +5,8 @@ import { navigation } from '../data';
 
 const NavMobile = () => {
   return (
-    <div className='bg-white w-full h-full shadow-2xl'>
-      <ul className='text-center h-full flex flex-col items-center justify-center gap-y-6'>
+    <div className='bg-white w-full rounded-lg shadow-xl'>
+      <ul className='text-center flex flex-col items-center justify-start gap-y-6'>
         {navigation.map((item, index) => {
           return (
             <li key={index}>
